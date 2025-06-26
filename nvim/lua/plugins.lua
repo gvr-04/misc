354de2a0d5bc768babcd8d54a087322eede7a96b
gummadi vamsidhar reddy <vamsidharr04@gmail.com>
2025-06-26 13:43:52 +0530

    removed snacks refer to prev commit for snacks.nvim config *-*

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,85 +1,19 @@
 return {
-
-	  -- snacks
-	  -- {
-	  -- 	"folke/snacks.nvim",
-	  -- 	priority = 1000,
-	  -- 	lazy = false,
-	  -- 	opts = {
-	-- 		bigfile = { enabled = true },
-	-- 		dashboard = { 
-	-- 			enabled = true, example = "advanced",
-	-- 			sections = {
-	-- 				{ section = "header" },
-	-- 				{
-	-- 					pane = 2,
-	--  				  	  	section = "terminal",
-	--  				  	  	cmd = "colorscript -e crunchbang-mini",
-	--  				  	  	height = 5,
-	--  				  	  	padding = 1,
-	--  				  	},
-	--  				  	{ section = "keys", gap = 1, padding = 5 },
-	--  				  	{ pane = 2, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-	--  				  	{ pane = 2, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-	--  				  	{
-	-- 					pane = 2,
-	--  				  	  	icon = " ",
-	--  				  	  	title = "Git status",
-	--  				  	  	section = "terminal",
-	--  				  	  	enabled = function()
-	-- 						return Snacks.git.get_root() ~= nil
-	--  				  	  	end,
-	--  				  	  	cmd = "git status --short --branch --renames",
-	--  				  	  	height = 5,
-	--  				  	  	padding = 1,
-	--  				  	  	ttl = 5 * 60,
-	--  				  	  	indent = 3,
-	--  				  	},
-	--  				  	{ section = "startup" },
-	--  				},
-	-- 		},
-	-- 		explorer = { enabled = true },
-	-- 		indent = { enabled = true },
-	-- 		input = { enabled = true },
-	-- 		picker = {
-	-- 			enabled = true,
-	-- 			sources = {
-	-- 				files = { hidden = true, ignored = true }
-	-- 			},
-	-- 			exclude = { ".local", ".cache", ".git", },
-	--
-	-- 		},
-	-- 		notifier = { enabled = true },
-	-- 		quickfile = { enabled = true },
-	-- 		scope = { enabled = true },
-	-- 		scroll = { enabled = true },
-	-- 		statuscolumn = { enabled = true },
-	-- 		words = { enabled = true },
-	-- 	},
-	-- 	keys = {
-	-- 		{ "<leader>p", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
-	-- 	    { "<leader>n",  function() Snacks.notifier.show_history() end, desc = "Notification History" },
-	-- 	    { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
-	-- 	    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
-	-- 		{ "<leader>t", function() Snacks.terminal() end, desc = "Toggle Terminal" },
-	-- 	},
-	-- },
-
 	-- blink
 	{
 		'saghen/blink.cmp',
 		dependencies = { 'rafamadriz/friendly-snippets' },
 		version = '1.*',
 		opts = {
-		    keymap = { preset = 'default' },
-		    appearance = {
+			keymap = { preset = 'default' },
+			appearance = {
 				nerd_font_variant = 'mono'
-		    },
-		    completion = { documentation = { auto_show = false } },
-		    sources = {
+			},
+			completion = { documentation = { auto_show = false } },
+			sources = {
 				default = { 'lsp', 'path', 'snippets', 'buffer' },
-		    },
-		    fuzzy = { implementation = "prefer_rust_with_warning" }
+			},
+			fuzzy = { implementation = "prefer_rust_with_warning" }
 		},
 		opts_extend = { "sources.default" }
 	},
@@ -87,9 +21,9 @@ return {
 	-- colorscheme switcher
 	{
 		"zaldih/themery.nvim",
-    	lazy = false,
+		lazy = false,
 		priority = 1000,
-  		opts = {
+		opts = {
 			themes = {"evergarden", "tokyonight", "retrobox" }, -- Your list of installed colorschemes.
 			livePreview = true,
 		},
@@ -106,25 +40,20 @@ return {
 				transparent_background = false,
 				sign = { color = 'none' },
 				float = { color = 'mantle', invert_border = false },
-	  	    	completion = { color = 'surface0' },
+				completion = { color = 'surface0' },
 			},
 		},
 	},
-	
+
 	-- colorscheme tokyonight
 	{
 		"folke/tokyonight.nvim",
-  		lazy = false,
-  		priority = 1000,
-  		opts = {},
+		lazy = false,
+		priority = 1000,
+		opts = {},
 	},
 
 	-- markdown
-	-- {
-	-- 	"OXY2DEV/markview.nvim",
-	-- 	priority = 1000,
-	-- 	lazy = false,
-	-- },
 	{
 		'MeanderingProgrammer/render-markdown.nvim',
 		dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
@@ -142,7 +71,7 @@ return {
 	-- lualine
 	{
 		'nvim-lualine/lualine.nvim',
-    	dependencies = { 'nvim-tree/nvim-web-devicons' },
+		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		config = function()
 			require('lualine').setup{
 				options = { 
@@ -154,7 +83,7 @@ return {
 					lualine_c = {
 						{ 'filename', path = 3 },
 					},
-					 lualine_x = {'fileformat', 'filetype'},
+					lualine_x = {'fileformat', 'filetype'},
 				},
 			}
 		end
@@ -165,14 +94,14 @@ return {
 		"folke/flash.nvim",
 		event = "VeryLazy",
 		---@type Flash.Config
- 	 	opts = {},
- 	 	-- stylua: ignore
- 	 	keys = {
- 	 	  { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
- 	 	  { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
- 	 	  { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
- 	 	  { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
- 	 	  { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
+		opts = {},
+		-- stylua: ignore
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
+			{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+			{ "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
+			{ "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
+			{ "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
 		},
 	},
 
@@ -182,43 +111,40 @@ return {
 		-- dependencies = { 'echasnovski/mini.icons' },
 		dependencies = { 'nvim-tree/nvim-web-devicons' },
 		config = function()
-		  local startify = require("alpha.themes.startify")
-		  startify.file_icons.provider = "devicons"
-		  require("alpha").setup(
-			startify.config
-		  )
+			local startify = require("alpha.themes.startify")
+			startify.file_icons.provider = "devicons"
+			require("alpha").setup(
+				startify.config
+			)
 		end,
-	 },
+	},
 
 	--neorg
 	{
 		"nvim-neorg/neorg",
-    	lazy = false,
-    	version = "*",
+		lazy = false,
+		version = "*",
 		config = function()
-        require("neorg").setup {
-          load = {
-            ["core.defaults"] = {},
-            ["core.concealer"] = {},
-            ["core.dirman"] = {
-              config = {
-                workspaces = {
-					home = "~/myhome",
-					projects = "~/projects",
-					note = "~/notes",
-                },
-                default_workspace = "note",
-              },
-            },
-          },
-        }
-  
-        vim.wo.foldlevel = 99
-        vim.wo.conceallevel = 2
-      end,
-	}
+			require("neorg").setup {
+				load = {
+					["core.defaults"] = {},
+					["core.concealer"] = {},
+					["core.dirman"] = {
+						config = {
+							workspaces = {
+								home = "~/myhome",
+								projects = "~/projects",
+								note = "~/notes",
+							},
+							default_workspace = "note",
+						},
+					},
+				},
+			}
+
+			vim.wo.foldlevel = 99
+			vim.wo.conceallevel = 2
+		end
+	},
 
 }
-
-
-
